37f0192cc260df5aa070150d5ddce45114c82e30
Peloshain <luisey.93@gmail.com>
2016-05-29 16:59:29 -0700

    x

---
+++ Models/Pago.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiExpress.Models
+{
+    public class Pago
+    {
+        public int Amount { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpirationDate { get; set; }
+        public string SecurityCode { get; set; }
+        public string Token { get; set; }
+    }
+}
---
+++ Servicios/EntregaServicio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Security;
+using ServiExpress.Models;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+
+
+namespace ServiExpress.Servicios
+{
+     
+    public class EntregaServicio
+    {
+        private ServicioDeEntregasEntities db = new ServicioDeEntregasEntities();
+
+        public List<tbEntregas> ObtenerLista()
+        {
+            return (from o in db.tbEntregas select o).ToList();
+        }
+
+        public tbEntregas ObtenerPorOrden(int idorden)
+        {
+            return (from o in db.tbEntregas where o.idOrden == idorden select o).FirstOrDefault();
+        }
+
+        public tbEntregas ObtenerOrdenPorIdCliente(int idCliente)
+        {
+            return (from o in db.tbEntregas where o.idCliente == idCliente select o).FirstOrDefault();
+        }
+         public string Crear(tbEntregas entrega)
+        {
+            try
+            {
+
+                db.tbEntregas.Attach(entrega);
+                db.Entry(entrega).State = EntityState.Added;
+                db.SaveChanges();
+                return "";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+    }
+}
+}
